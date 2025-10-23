f8d1057cc437ad5cfa322f9b8823033d89f32002
ecemergul <101472525+ecemergul@users.noreply.github.com>
2025-10-23 10:22:38 +0300

    Update Lambda function

--- lambdas/welcome_function/welcome_function.py
+++ lambdas/welcome_function/welcome_function.py
@@ -2,5 +2,6 @@ def lambda_handler(event, context):
     return {
         "statusCode": 200,
         "headers": {"Content-Type": "text/html"},
-        "body": "<h1>Merhaba, teleskop projesine hoş geldiniz!</h1>"
+        "body": "<h1>Merhaba, teleskop projesine hoş geldiniz!</h1>",
+        "isBase64Encoded": False
     }
